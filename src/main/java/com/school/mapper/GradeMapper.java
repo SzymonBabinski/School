804178f7b1d3a220a5291823085bec076e8c8b92
Szymon Babiński <szymon.babinski@student.put.poznan.pl>
2021-03-17 23:21:09 +0100

    Remove service from mapper

--- src/main/java/com/school/mapper/GradeMapper.java
+++ src/main/java/com/school/mapper/GradeMapper.java
@@ -2,7 +2,7 @@ package com.school.mapper;
 
 import com.school.dto.GradeDto;
 import com.school.model.Grade;
-import com.school.service.StudentService;
+import com.school.model.Student;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -11,34 +11,15 @@ import java.util.stream.Collectors;
 @Component
 public class GradeMapper {
 
-    private StudentService studentService;
-
-    public GradeMapper(StudentService studentService) {
-        this.studentService = studentService;
-    }
-
-    public Grade mapToGrade(final GradeDto gradeDto, int studentId) {
-        return new Grade(
-                gradeDto.getValue(),
-                gradeDto.getSubject(),
-                studentService.findStudentById(studentId).get()
-        );
+    public Grade mapToGrade(final GradeDto gradeDto, Student student) {
+        return new Grade(gradeDto.getValue(), gradeDto.getSubject(), student);
     }
 
     public GradeDto mapToGradeDto(final Grade grade) {
-        return new GradeDto(
-                grade.getId(),
-                grade.getValue(),
-                grade.getSubject(),
-                grade.getStudent().getId()
-        );
+        return new GradeDto(grade.getId(), grade.getValue(), grade.getSubject(), grade.getStudent().getId());
     }
 
     public List<GradeDto> mapToGradeDto(final List<Grade> gradeList) {
-        return gradeList
-                .stream()
-                .map(this::mapToGradeDto)
-                .collect(Collectors.toList());
+        return gradeList.stream().map(this::mapToGradeDto).collect(Collectors.toList());
     }
 }
-
